2f7a2a0e284c431c841490eea88b86d81db3b988
cfarkas <carlosfarkas@gmail.com>
2020-04-04 10:04:16 -0600

    Update genome_download.sh

--- bash_scripts/genome_download.sh
+++ bash_scripts/genome_download.sh
@@ -4,51 +4,51 @@ genome=${1}
 
 if [ "$1" == "-h" ]; then
   echo ""
-  echo "Usage: bash ./`basename $0` {genome}"
+  echo "Usage: ./`basename $0` [genome]"
   echo ""
-  echo "This script will download and index the specified genome from the uscs server (goldenpath) including annotation"
+  echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "{genome}: Name of the genome assembly"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
 
 if [ "$1" == "-help" ]; then
   echo ""
-  echo "Usage: bash ./`basename $0` {genome}"
+  echo "Usage: ./`basename $0` [genome]"
   echo ""
-  echo "This script will download and index the specified genome from the uscs server (goldenpath) including annotation"
+  echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "{genome}: Name of the genome assembly"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
 if [ "$1" == "--h" ]; then
   echo ""
-  echo "Usage: bash ./`basename $0` {genome}"
+  echo "Usage: ./`basename $0` [genome]"
   echo ""
-  echo "This script will download and index the specified genome from the uscs server (goldenpath) including annotation"
+  echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "{genome}: Name of the genome assembly"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
 
 if [ "$1" == "--help" ]; then
   echo ""
-  echo "Usage: bash ./`basename $0` {genome}"
+  echo "Usage: ./`basename $0` [genome]"
   echo ""
-  echo "This script will download and index the specified genome from the uscs server (goldenpath) including annotation"
+  echo "This program will download and index the specified genome from the uscs server (goldenpath) including annotation"
   echo ""
-  echo "{genome}: Name of the genome assembly"
+  echo "[genome]: Name of the genome assembly"
   echo ""
   exit 0
 fi
 
-[ $# -eq 0 ] && { echo "Usage: bash ./`basename $0` {genome}"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: ./`basename $0` [genome]"; exit 1; }
 
 if [ $# -ne 1 ]; then
-  echo 1>&2 "Usage: bash ./`basename $0`  {genome}"
+  echo 1>&2 "Usage: ./`basename $0`  [genome]"
   exit 3
 fi
 
